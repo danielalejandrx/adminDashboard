1032a8b7025b7d016fa0853c176d0ea0b64f8d26
Andy <danielalejandra.css@gmail.com>
2023-06-22 19:40:16 -0400

    ahora zi

--- src/componenetes/TopContainers.jsx
+++ src/componenetes/TopContainers.jsx
@@ -1,55 +1,55 @@
-import React, {useEffect} from "react";
-import { BiSearchAlt2 } from "react-icons/bi";
+import React, { useEffect } from "react";
+import { BiSearchAlt } from "react-icons/bi";
 import { FaBell, FaChevronDown } from "react-icons/fa";
+import perfil from "../img/perfil.png";
 
-function TopContainers() {
+function TopContainer() {
+  useEffect(() => {
+    const mouseTarget = document.getElementById("menuChevron");
+    const menuContainer = document.getElementById("menuContainer");
+    mouseTarget.addEventListener("mouseenter", () => {
+      mouseTarget.style.transform = "rotate(180deg)";
+      menuContainer.style.transform = "translateX(0px)";
+    });
 
-    useEffect(() =>{
-        const menuTarget = document.getElementById("menuChevron");
-        const menuContainer = document.getElementById("menuContainer");
-
-        menuTarget.addEventListener("mouseenter", () =>{
-            menuContainer.style.transform = 'translatex(300px)'
-        });
-
-        menuTarget.addEventListener("mousele", () => {
-            console.log("hi");
-        });
-    }, []);
+    menuContainer.addEventListener("mouseleave", () => {
+      mouseTarget.style.transform = "rotate(0deg)";
+      menuContainer.style.transform = "translateX(300px)";
+    });
+  }, []);
 
   return (
     <div className="topContainer">
       <div className="inputBox">
-        <input type="text" placeholder="Buscar" />
-        <i className="lupa">
-          <BiSearchAlt2 />
+        <input type="text" placeholder="Search items, collections" />
+        <i>
+          <BiSearchAlt />
         </i>
       </div>
+
       <div className="profileContainer">
         <i className="profileIcon">
           <FaBell />
         </i>
-
         <div className="profileImage">
-            <img src={perfil} alt="" />
+          <img src={perfil} alt="" />
         </div>
-
-        <p className="prfileName"> andy </p>
+        <p className="profileName">Melon uwu</p>
         <i className="menuChevron" id="menuChevron">
           <FaChevronDown />
         </i>
-        <div className="menuContainer" id="menuContainer">
-            <ul>
-                <li>Web design</li>
-                <li>UI / UX</li>
-                <li>Form Design</li>
-                <li>UI design</li>
-            </ul>
 
+        <div className="menuContainer" id="menuContainer">
+          <ul>
+            <li>Web design</li>
+            <li>UI / UX</li>
+            <li>Form Design</li>
+            <li>UI design</li>
+          </ul>
         </div>
       </div>
     </div>
   );
 }
 
-export default TopContainers;
+export default TopContainer;
