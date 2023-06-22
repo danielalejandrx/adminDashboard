5def3e65930b4381000d555adb0d93539eff0336
Andy <danielalejandra.css@gmail.com>
2023-06-22 15:58:42 -0400

    me acorde

--- src/componenetes/TopContainers.jsx
+++ src/componenetes/TopContainers.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { BiSearchAlt2 } from "react-icons/bi";
 import { FaBell, FaChevronDown } from "react-icons/fa";
+import perfil from "../img/perfil.png";
 
 function TopContainers() {
+
+    useEffect(() =>{
+        const menuTarget = document.getElementById("menuChevron");
+        const menuContainer = document.getElementById("menuContainer");
+
+        menuTarget.addEventListener("mouseenter", () =>{
+            menuContainer.style.transform = 'translatex(300px)'
+        });
+
+        menuTarget.addEventListener("mousele", () => {
+            console.log("hi");
+        });
+    }, []);
+
   return (
     <div className="topContainer">
       <div className="inputBox">
@@ -16,12 +31,23 @@ function TopContainers() {
           <FaBell />
         </i>
 
-        {/* <div className="profileImage"></div> */}
+        <div className="profileImage">
+            <img src={perfil} alt="" />
+        </div>
 
         <p className="prfileName"> andy </p>
         <i className="menuChevron" id="menuChevron">
           <FaChevronDown />
         </i>
+        <div className="menuContainer" id="menuContainer">
+            <ul>
+                <li>Web design</li>
+                <li>UI / UX</li>
+                <li>Form Design</li>
+                <li>UI design</li>
+            </ul>
+
+        </div>
       </div>
     </div>
   );
